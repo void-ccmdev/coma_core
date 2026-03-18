1a38f1b6fb8e24bafb8d3260856234cc8fe4a877
ccmdev <controlcommanddev@gmail.com>
2026-03-18 15:21:20 +0100

    Added Flashlight Script and updated kw_debug.tscn

---
+++ scripts/player/flashlight.gd
@@ -0,0 +1,33 @@
+class_name Flashlight extends SpotLight3D
+
+
+@export var on : bool = true
+
+@export_group("Settings")
+@export var parent : Camera3D
+@export var enable_smoothing : bool = true
+@export var smoothing_power : float = 0.125 #Default
+
+func _ready() -> void:
+	print("Flashlight Script Loaded!")
+
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("flashlight"):
+		if on:
+			self.hide()
+			on = false
+		else:
+			self.show()
+			on = true
+
+func _process(_delta: float) -> void:
+	if enable_smoothing:
+		self.top_level = true
+		get_tree().create_tween().tween_property(
+												self,
+												"global_transform", 
+												parent.global_transform, 
+												smoothing_power
+											)
+	else:
+		self.top_level = false
