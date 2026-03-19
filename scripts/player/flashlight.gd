b07b27bb8dfbd9a5d890eaa4e42b41ea62f81fb9
ccmdev <controlcommanddev@gmail.com>
2026-03-19 20:54:35 +0100

    Added KICKBACK to kw_debug.tscn and added flashlight click

--- scripts/player/flashlight.gd
+++ scripts/player/flashlight.gd
@@ -7,6 +7,8 @@ class_name Flashlight extends SpotLight3D
 @export var parent : Camera3D
 @export var enable_smoothing : bool = true
 @export var smoothing_power : float = 0.125 #Default
+@export var use_audio : bool = true
+@export var audio_node : AudioStreamPlayer3D
 
 func _ready() -> void:
 	print("Flashlight Script Loaded!")
@@ -16,15 +18,23 @@ func _ready() -> void:
 	else:
 		self.hide()
 
-			
+func fl_click() -> void:
+	if use_audio && audio_node:
+		var random = randf_range(0.85, 1.15)
+		audio_node.pitch_scale = random
+		audio_node.stop()
+		audio_node.play()
+		
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("flashlight"):
 		if on:
 			self.hide()
 			on = false
+			fl_click()
 		else:
 			self.show()
 			on = true
+			fl_click()
 
 func _process(_delta: float) -> void:
 	if enable_smoothing:
