aac13772aad5b2c2799fe4f5c2f68cbbfaec2fd1
ccmdev <controlcommanddev@gmail.com>
2026-03-27 14:55:34 +0100

    Add interact system and debug interactable
    Introduce a simple interaction system and a debug interactable object.
    
    - Add InteractRay class (scripts/player/interact_ray.gd) and wire it into player.tscn as a RayCast node that references UI crosshair and message labels. Also add font and LabelSettings and new UI nodes (Crosshair, InteractMessage) to the player scene.
    - Add a small debug interactable StaticBody3D script (scripts/debug_interactable_obj.gd) with an exported message, and include it in kw_level_example.tscn as a test object (mesh + shape).
    - Mark relevant scene nodes with the "Interactable" group and add that group to project.godot so the raycast can detect interactable objects.
    - Minor scene/resource adjustments (DirectionalLight3D and resource uids) to support the new elements.
    
    This enables showing a crosshair and an on-screen message when the player aims at objects in the Interactable group.

---
+++ scripts/debug_interactable_obj.gd
@@ -0,0 +1,3 @@
+extends StaticBody3D
+
+@export var message : String = "BOX"
---
+++ scripts/player/interact_ray.gd
@@ -0,0 +1,17 @@
+class_name InteractRay extends RayCast3D
+
+var collider = null
+
+@export var ui_crosshair : Control
+@export var ui_message : Label
+
+func _process(_delta: float) -> void:
+	if self.is_colliding() && self.get_collider().is_in_group("Interactable"):
+		collider = self.get_collider()
+
+		ui_crosshair.show()
+		ui_message.text = collider.message
+	else:
+		collider = null
+		ui_crosshair.hide()
+		ui_message.text = ""
